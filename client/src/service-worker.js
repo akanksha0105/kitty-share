6f8a9b3fedf5de92bd4093ffed96e62d3d9e3595
Akanksha Mittal <mittalakanksha0105@gmail.com>
2022-01-08 20:34:00 +0530

    Fixing SW

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,4 +1,3 @@
-let url;
 const ignored = self.__WB_MANIFEST;
 console.log("I am there. My name is Service Worker");
 
@@ -32,7 +31,27 @@ self.addEventListener("push", (event) => {
 self.addEventListener("notificationclick", (event) => {
 	console.log("On notification click");
 
-	// event.notification.close();
+	var notification = event.notification;
+
+	console.log("Closed notification: " + notification);
+
+	event.notification.close();
+	let x = validURL(url);
+
+	console.log("x is : ", x);
 
 	clients.openWindow(url);
 });
+
+function validURL(str) {
+	var pattern = new RegExp(
+		"^(https?:\\/\\/)?" + // protocol
+			"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+			"((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+			"(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+			"(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+			"(\\#[-a-z\\d_]*)?$",
+		"i",
+	); // fragment locator
+	return !!pattern.test(str);
+}
